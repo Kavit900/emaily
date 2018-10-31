fc3a7aad00ee11522546f69f8509bbb0d6bc1d25
Kavit <kavitmht@gmail.com>
2018-10-31 15:08:14 -0700

    created google developers app oauthId

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({bye: 'buddy'});
-});
+passport.use(new GoogleStrategy());
 
 // Dynamically bind port to looki into the underlying environment, like in heroku we can fetch the port to be used
 const PORT = process.env.PORT || 5000;
